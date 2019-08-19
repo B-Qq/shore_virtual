9af1dad2967575f15cbbf8c5e6164c506031c540
Bq <bq5773718@163.com>
2019-08-19 14:05:56 +0800

    new

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
+'''
 username = input('请输入用户名:')
 password = input('请输入密码:')
 if username == 'admin' and password == 'admin':
     print('身份认证成功')
 else:
     print('身份认证失败')
+'''
+
+
+value  = float(input("请输入长度:"))
+unit = input("请输入长度")
+if unit == 'in' or unit == '英寸':
+    print(2111)
+elif unit == 'cm' or unit == '厘米':
+    print(22222)
+else:
+    print(3333)
